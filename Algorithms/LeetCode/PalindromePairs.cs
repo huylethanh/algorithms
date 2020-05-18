cd4170769a2f070c1c14b170d5814f81a1d70b9e
Huy Le Thanh <huy.thanhle@crimsonwing.com>
2020-05-18 12:03:35 +0200

    add other method

--- Algorithms/LeetCode/PalindromePairs.cs
+++ Algorithms/LeetCode/PalindromePairs.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Algorithms.LeetCode
 {
@@ -22,11 +23,11 @@ namespace Algorithms.LeetCode
         {
             var inputs = new List<string[]>
             {
+                new string[] { "abcd", "dcba", "lls", "s", "sssll" },
                 new string[] { "hfjd", "jfhajaadieahg", "fdjddhfdcfdcehi", "gfejeacaeacig", "efadjahgbjffi", "ehfffii", "dhgccjcghjdhjhjdai", "dccfcdiagjbideh", "ifbajfifdea", "cdhbjhhh", "bjcjidhfg", "jjaaihiegcaibhgg", "dhghiiegceghgdabia", "hihiadajddiiegc", "agcicaecieaciaaigd", "g", "bdhfiheajbgf", "degdddbfeejdcbfiif", "adifdgjcade", "chfjdgdgfdcbfihbfe", "bfcahadcbebdjieigdhc", "jfgegjhibecjjj", "hciiggehjbghfgjhj", "cc", "ffjgeji", "ibejefbjhf", "ehj", "jacg", "feagcjbgefjbefaf", "caeijg", "ihbhcddh", "hdhbhfacccecciffj", "cjjebfeefdiddahha", "gcdcgjbeieee", "digghjdcfja", "bagafhf", "c", "addgifjif", "ieehbe", "aiefibeececa", "fjhafhcehdehj", "iffdbegbbjggcghadhh", "jja", "igeejifibagfbhaghhaf", "d", "giaijchiaa", "iefdiicjafhebahcd", "chc", "hebff", "ifegfd", "geaficbdhgicdicbj", "ehigeifabgfhjgbi", "eije", "jdefbdggjbgefjehbge", "aeibjeaaj", "ejjhcdeddhabaacahi", "cfgabfeghchegdjj", "deadgbccebahd", "efccbehbfdddei", "ifi", "agb", "dacjedcchdjid", "gdeji", "bfijdhebbdcai", "ecdhjhfafbb", "cchejjgfhbf", "diieggbfgjfefdbaifi", "jggjdejbcgdicjgfj", "egi", "ih", "jjchdiebja", "feijafjiahhabcefficd", "fgbbibgaffffcbjgccjj", "bedghiidach", "ecehfejeegadjdibdfac", "ijgcbhiaeec", "bfieifa", "bhaaeag", "cgaaideef", "abhhbg", "dcggabdbgebhgacffeee", "eabhfbbbegjhhef", "bib", "ghgdbbbeegjcc", "bheegi", "i", "gcafhfibafdd", "hbdidcdbjibcceee", "hdcgic", "jc", "ide", "gjfbfiiggjfgfeiib", "hcbgeibgbfdjecdhcfci", "hdbjfefgjiafbacgife", "ecfcigeiiaad", "ebfh", "ahecfgacfffdabj", "hgidgdghj", "aiiciccheiebfi", "hjachjbiiaahdgi", "jcdib", "giceadhg", "jaedhaegjejeh", "headgicfcfgafahdi", "bjfbaihhc", "ecjfhfcdfih", "jibcjhajfajibhcj", "ffdagjcefihjidc", "hbcdhecfecdhcbgaacbh", "jid", "cjibig", "cahhijeafiibgadaihe", "fieedidgcjej", "fcea", "becagjgehf", "ba", "jggijbbdcfeecejiicgh", "abjfdfaebcbiefhb", "dhdbbgjegdegfiagb", "ebgafjjeibegjg", "igfcjjehbhga", "ehciihj", "cjgadbfhaaibihcacjid", "ghchcfchegibc", "ddjbheajfcgcca", "cji", "cbhhjceijfigaibjchhe", "ihbccggjggeb", "ecdcicfhjbbbc", "dibbbfha", "jhcdjibibagdeedjcchi", "ecejijcddjbgfjjaji", "hjfjhj", "edeacfdhjd", "af", "eghihebeg", "ifaedbdibifigfeje", "ghjfejeghebi", "ahcd", "ehh", "iihfhbjcij", "ghaccchhbgabaacg", "diihgbhcbbfgagidb", "iafbid", "aehhdbcja", "aagcggbjfdchahihab", "ijhahgfce", "iahhiigcaddhf", "jhejjeghabi", "ebfbeaifehdache", "ca", "haigedibehbjhja", "ejgfbb", "efgihgidh", "edede", "hjaii", "dicdddjafedaa", "abjcghdhgaaabaaccbic", "acbjchabadajcheddbh", "hgcjchhjdfi", "fhfidbajiaceeific", "eddghiahfijjb", "hbaejbh", "ghcdijacbcfichbjedh", "gefcbgaefafgadigfij", "jjaib", "dcdhgejedcg", "hfebhjeabehbhaaijhi", "ghd", "cagheihiaigc", "bd", "dccgbebbbai", "f", "bdbhedigci", "gdciehiifefibeigfgh", "icejeajchfgbiccehi", "eahgb", "jjfebhjhgghe", "eaajbihccag", "ijhjhfabccdfaghd", "jcffihgdfd", "hefh", "ahbgfcecagiahfa", "cigffhgeijh", "ghcdhdgfgdhfbijgb", "bdgdbeefiajehcf", "a", "gcjidfd", "jbbeiicahdc", "gdffi", "gdcbagjfice", "bfiigabciijcgaiih", "hafeedebdif", "efeaeigdaiihja", "edjcgae", "gcehf", "aebjcfcdiagee", "jddci", "gejhe", "bdahiicijibahfgbid", "afccgaihideghi", "eedfd", "abafh", "fgdgjddibjcejdejg", "fjedhfcbeffegcbhaadj", "jhicdbdiehddechf", "jjgbbfdbgeegcaia", "bcah", "ghhgecaajbbcjge", "ajagbhajgbadhhibg", "hbeggafgbai", "cbjhgcgaffbdhb", "jib", "jcjjfiebd", "fgbhffaig", "e", "iefgbcadafbbbgjci", "ihdhbjgcca", "gefifbgcajcabef", "ijbeicahfafagabiedg", "iaceiabjd", "chcejbai", "edjjajiccgabghdgbei", "ghgdbcdccbgfefd", "iie", "cdgejcabbcbhgjcjec", "jbgcfbfbbeagajh", "dccbb", "ihcjhfjcfghgg", "cahhdaeheaejfic", "bgahebhbgbhdagj", "gag", "aeihfjadea", "bbaigaj", "gicic", "fiecehabaajhid", "cjghjdbdegbaiageifce", "gdgcf", "dddbjbi", "bgjhdjeefbc", "difgffciafjbbfiagf", "icahdfedabffhacg", "hjhae", "hhgbabc", "abbggaeibacibjafbg", "gjdgfcj", "cffchjjeeachefcjjh", "bhcbg", "hjfjfaehjdj", "gjcdgjjhaji", "ihfajdc", "jichbjiaddadgibj", "dgjefhjij", "dhaed", "gghgcjje", "jefajijciffigceaj", "jccjijc", "hijbgddhccheaeaadjif", "jhghabejdhcbdbfjg", "jbjicjgagbejhbf", "cbdb", "hcgeghfeai", "iecigchdgdhhii", "deaihbejgbe", "ejcgggebahibhiheai", "jfdghjdihafgheeidchc", "agibbfigaejfia", "ifegddacdcjej", "cjhafiebhai", "dbh", "bbdifjbe", "cbegh", "jbffbjeffhdbdib", "fcbbgdjdg", "hhidcjjc", "bgidbagebbjcgfdccdcc", "gaidc", "jdfeajcafcdhec", "fda", "ihfaa", "babhbahbicdiefea", "fceiafjfchcj", "afijgeeccghdiaa", "hhjcdefjbfcgjcf", "ciaeji", "jhdedfj", "bcfdjhfdhdafgfjc", "ibbiaicafai", "dgcicgafc", "affjbchhicdacejjgde", "ej", "cicahbhac", "ddjdcddiab", "aaj", "igfedigjgdgd", "cbecjgeeag", "fjhfdfbb", "hcdghhcjfjg", "ccbefai", "chibjd", "baghechiiieiighifi", "egahgebhihcdghjaffah", "gchgbeghabehheeigid", "dcdejidcb", "gfgjafii", "agabjgehaadf", "achegfgihhij", "aibcifjjeahhb", "hc", "hebiaa", "hahcfadgg", "cdgcaghidgfifgibfba", "jjbjijecichjjccje", "jcjeiejga", "b", "bb", "gj", "cbgceh", "befde", "cidegbgagf", "dafbb", "geeafdbjjebfeiieeb", "hhjdcafeaebbach", "cffggdeeih", "jfddjejhehe", "biibchbcigcee", "ibjjaah", "igjjafgffded", "hgbj", "fbhfaajhgicgeiajibdf", "gfibhaccjfh", "bhhhaejhf", "bebf", "gfcbjfadfehegiibjha", "jbihhcfgjbggi", "ecbfhicbfijf", "djdjehgifei", "j", "bdfaicdjhgdihcdi", "djaaddigeeifbciehd", "cchafje", "ibhdeade", "fggicjcaiee", "afebjfbe", "fchfcjfeaafej", "dcegjia", "hjjbaggb", "hcebbchdechfj", "cbi", "ejgdfgfbgbgahaeihb", "fiiibcfjajhdafhha", "fgjab", "cidc", "gecd", "bijgffgfjbfdecjii", "eigghcbb", "fefcbffeihijehc", "dfahab", "gffagff", "fgebcghiajcdafha", "hdi", "fghfjia", "eejcfhhcdjcdajeji", "jgca", "aj", "ei", "ejcdh", "chdfdfahehdgii", "eihgjaeagcjbjdbiib", "ahjghheecifc", "hggeeacfhceidgifbag", "giebhgebeafigbgbigc", "gbcif", "cebcbeefbica", "ggghca", "gceaicb", "fgjdjih", "eehcdajadh", "fchgdjhibjjchcccci", "bdcdbbefbabjj", "deeabgdibccige", "dadhdifjccig", "dgja", "ghcahfdccbbbegdgi", "bc", "df", "jhaebbeeagf", "dajjjcdfiaifcaachhj", "jhefbhgbgchbiiea", "aagdjhdibabfbbebe", "bggiaaeje", "jhgejdg", "dgjbdgbdchgf", "hea", "bacicfcjiifceabecfe", "afaahjde", "deebachgigahbac", "edideehfgjbbgehgi", "id", "cgcffjejhdacfcccbijj", "ahghcghfchfbdegdb", "bhcdaijiihff", "jddhg", "eeaffdibdeefgfd", "dgdbfhaigagbbdbjd", "ib", "cdfa", "jfbhbiibiebffc", "jigdbjcbjgedjajadb", "chdaddfgifbi", "da", "dfjedajghfbbfgebb", "cfjihhccdibegeccgjhg", "aheegacijjgdgaicehfi", "ahhijdgf", "bgadfbaaagija", "hibhebbcgfg", "cefhbcahadijhgacidaa", "acaegibddceicjafbbcj", "bhda", "gjgahdjgihbfedhiadch", "fbd", "ihcagdigiibggd", "cdjdbdebciehifccbdf", "heedhhchbhgabjafib", "cjf", "chdgjcgcejjid", "bf", "hffihgahhhbgehjdg", "igbjehacfhagicd", "bjefhgfcighfc", "jhabf", "ebacbcfchgigjbbged", "hihbhgigjj", "ebieeichifeicjce", "gijbhebddefcgbcg", "ggj", "igfddeffiadgejh", "gdfiidfgadbaifcihai", "echghadhjaiejda", "jebjbagagjjhcdfb", "igifdccbii", "agbjiajhbcbfachbd", "bfcidhabefjjj", "afeeghcicbcfbgj", "fff", "fcccjbjhhhbabiafdfj", "hbcgcehibcdefagcf", "gghdeea", "ecjfffege", "ihifdbe", "cfiicegii", "ighddeiheacjddgd", "hicjfaebcjaeecddhch", "jdcdjfjbhaefehahhh", "cjagdcibf", "jhddjdh", "aaefbaggcdigehfhifii", "idaiah", "edbf", "ciaddfcccgae", "fcebgaajahbfeffcidd", "dfijdgeghh", "dfjfdf", "eaggbeeidiiacb", "fc", "bjibjccdfbdf", "jii", "jijgfbg", "fi", "fedjchdcghifjife", "egjfibhgahi", "jcf", "aeciia", "edhb", "eai", "ficcdffe", "hiedjedadiahhcbd", "jdjihcfahciheaciaahd", "bjcabjcgjhfbbhbagc", "ajghc", "iaaeajfbbadeeejfh", "hfciaeacjc", "igaefcdda", "ccad", "djhji", "jagai", "hdheidi", "aeecab", "aidjfh" },
                 new string[] { "aabaa", "" },
                 new string[] { "a", "abc", "aba", "" },
                 new string[] { "a", "" },
-                new string[] { "abcd", "dcba", "lls", "s", "sssll" },
                 new string[] { "bat", "tab", "cat" }
             };
 
@@ -34,7 +35,7 @@ namespace Algorithms.LeetCode
 
             foreach (var words in inputs)
             {
-                var value = DoPalindromePairs(words);
+                var value = PalindromePairs2(words);
 
                 foreach (var item in value)
                 {
@@ -109,6 +110,73 @@ namespace Algorithms.LeetCode
             var result = string.Equals(partA, resvert);
 
             return result;
+        }
+
+
+        public IList<IList<int>> PalindromePairs2(String[] words)
+        {
+            IList<IList<int>> ret = new List<IList<int>>();
+
+            if (words == null || words.Length < 2)
+            {
+                return ret;
+            }
+
+            Dictionary<string, int> map = new Dictionary<string, int>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                map.Add(words[i], i);
+            }
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = 0; j <= words[i].Length; j++)
+                {
+                    // notice it should be "j <= words[i].Length()"
+                    string str1 = words[i].Substring(0, j);
+                    string str2 = words[i].Substring(j);
+                    
+                    if (isPalindrome(str1))
+                    {
+                        string str2rvs = new string(str2.Reverse().ToArray());
+
+                        if (map.ContainsKey(str2rvs) && map[str2rvs] != i)
+                        {
+                            List<int> list = new List<int>();
+                            list.Add(map[str2rvs]);
+                            list.Add(i);
+                            ret.Add(list);
+                        }
+                    }
+
+                    if (isPalindrome(str2))
+                    {
+                        string str1rvs = new string(str1.Reverse().ToArray());
+                        // check "str.Length() != 0" to avoid duplicates
+                        if (map.ContainsKey(str1rvs) && map[str1rvs] != i && str2.Length != 0)
+                        {
+                            List<int> list = new List<int>();
+                            list.Add(i);
+                            list.Add(map[str1rvs]);
+                            ret.Add(list);
+                        }
+                    }
+                }
+            }
+
+            return ret;
+        }
+
+        private bool isPalindrome(String str)
+        {
+            int left = 0;
+            int right = str.Length - 1;
+            while (left <= right)
+            {
+                if (str[left++] != str[right--]) return false;
+            }
+
+            return true;
         }
     }
 }
